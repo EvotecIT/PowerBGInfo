2f6652897b381936dd1f1d2574d015f168d075e9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-29 20:04:55 +0100

    Add initial package

---
+++ PowerBGInfo.psd1
@@ -0,0 +1,22 @@
+﻿@{
+    AliasesToExport        = @()
+    Author                 = 'Przemyslaw Klys'
+    CmdletsToExport        = @()
+    CompanyName            = 'Evotec'
+    CompatiblePSEditions   = @('Desktop', 'Core')
+    Copyright              = '(c) 2011 - 2022 Przemyslaw Klys @ Evotec. All rights reserved.'
+    Description            = 'PowerBGInfo is a module that allows you to create background images with information about your environment.'
+    DotNetFrameworkVersion = '4.7.2'
+    FunctionsToExport      = 'New-BGInfo'
+    GUID                   = '91b9c52d-6a39-4a65-a276-409b9390ee04'
+    ModuleVersion          = '0.0.1'
+    PowerShellVersion      = '5.1'
+    PrivateData            = @{
+        PSData = @{
+            Tags       = @('windows', 'image', 'charts', 'qrcodes', 'barcodes')
+            LicenseUri = 'https://github.com/EvotecIT/PowerBGInfo/blob/master/License'
+            ProjectUri = 'https://github.com/EvotecIT/PowerBGInfo'
+        }
+    }
+    RootModule             = 'PowerBGInfo.psm1'
+}
---
+++ PowerBGInfo.psm1
@@ -0,0 +1,59 @@
+#Get public and private function definition files.
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+$Classes = @( Get-ChildItem -Path $PSScriptRoot\Classes\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+$Enums = @( Get-ChildItem -Path $PSScriptRoot\Enums\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+
+$AssemblyFolders = Get-ChildItem -Path $PSScriptRoot\Lib -Directory -ErrorAction SilentlyContinue
+$Assembly = @(
+    if ($AssemblyFolders.BaseName -contains 'Standard') {
+        @( Get-ChildItem -Path $PSScriptRoot\Lib\Standard\*.dll -ErrorAction SilentlyContinue -Recurse)
+    }
+    if ($PSEdition -eq 'Core') {
+        @( Get-ChildItem -Path $PSScriptRoot\Lib\Core\*.dll -ErrorAction SilentlyContinue -Recurse )
+    } else {
+        @( Get-ChildItem -Path $PSScriptRoot\Lib\Default\*.dll -ErrorAction SilentlyContinue -Recurse )
+    }
+)
+$FoundErrors = @(
+    Foreach ($Import in @($Assembly)) {
+        try {
+            Write-Verbose -Message $Import.FullName
+            Add-Type -Path $Import.Fullname -ErrorAction Stop
+            #  }
+        } catch [System.Reflection.ReflectionTypeLoadException] {
+            Write-Warning "Processing $($Import.Name) Exception: $($_.Exception.Message)"
+            $LoaderExceptions = $($_.Exception.LoaderExceptions) | Sort-Object -Unique
+            foreach ($E in $LoaderExceptions) {
+                Write-Warning "Processing $($Import.Name) LoaderExceptions: $($E.Message)"
+            }
+            $true
+            #Write-Error -Message "StackTrace: $($_.Exception.StackTrace)"
+        } catch {
+            Write-Warning "Processing $($Import.Name) Exception: $($_.Exception.Message)"
+            $LoaderExceptions = $($_.Exception.LoaderExceptions) | Sort-Object -Unique
+            foreach ($E in $LoaderExceptions) {
+                Write-Warning "Processing $($Import.Name) LoaderExceptions: $($E.Message)"
+            }
+            $true
+            #Write-Error -Message "StackTrace: $($_.Exception.StackTrace)"
+        }
+    }
+    #Dot source the files
+    Foreach ($Import in @($Private + $Public + $Classes + $Enums)) {
+        Try {
+            . $Import.Fullname
+        } Catch {
+            Write-Error -Message "Failed to import functions from $($import.Fullname): $_"
+            $true
+        }
+    }
+)
+
+if ($FoundErrors.Count -gt 0) {
+    $ModuleName = (Get-ChildItem $PSScriptRoot\*.psd1).BaseName
+    Write-Warning "Importing module $ModuleName failed. Fix errors before continuing."
+    break
+}
+
+Export-ModuleMember -Function '*' -Alias '*'
