72062963f15b34c30d20bf25667bb917bcf3ab9a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-29 23:04:03 +0100

    Add cmds to create bginfo

---
+++ Public/New-BGInfoLabel.ps1
@@ -0,0 +1,16 @@
+﻿function New-BGInfoLabel {
+    [CmdletBinding()]
+    param(
+        [string] $Name,
+        [SixLabors.ImageSharp.Color] $Color,
+        [float] $FontSize,
+        [string] $FontFamilyName
+    )
+    [PSCustomObject] @{
+        Type           = 'Label'
+        Name           = $Name
+        Color          = $Color
+        FontSize       = $FontSize
+        FontFamilyName = $FontFamilyName
+    }
+}
---
+++ Public/New-BGInfoValue.ps1
@@ -0,0 +1,19 @@
+﻿function New-BGInfoValue {
+    [CmdletBinding()]
+    param(
+        [string] $Name,
+        [string] $Value,
+        [SixLabors.ImageSharp.Color] $Color,
+        [float] $FontSize,
+        [string] $FontFamilyName
+    )
+
+    [PSCustomObject] @{
+        Type           = 'Values'
+        Name           = $Name
+        Value          = $Value
+        Color          = $Color
+        FontSize       = $FontSize
+        FontFamilyName = $FontFamilyName
+    }
+}
