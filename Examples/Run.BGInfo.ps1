f70d0d4b1dae51f9c6b493ac706949c6c88e6dfc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-30 09:41:13 +0100

    Example update

---
+++ Examples/Run.BGInfo.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module .\PowerBGInfo.psd1 -Force
+
+New-BGInfo -MonitorIndex 0 {
+    # Lets add computer name
+    New-BGInfoValue -Name 'ComputerName' -Value $env:COMPUTERNAME -Color Red -FontSize 16 -FontFamilyName 'Calibri'
+
+    # Lets add Label, but without any values
+    New-BGInfoLabel -Name "Drives" -Color LemonChiffon -FontSize 16 -FontFamilyName 'Calibri'
+
+    # Lets get all drives and their labels
+    foreach ($Disk in (Get-Disk)) {
+        $Volumes = $Disk | Get-Partition | Get-Volume
+        foreach ($V in $Volumes) {
+            New-BGInfoValue -Name "Drive $($V.DriveLetter)" -Value $V.SizeRemaining -Color Red -FontSize 16 -FontFamilyName 'Calibri'
+        }
+    }
+} -FilePath $PSScriptRoot\Samples\PrzemyslawKlysAndKulkozaurr.jpg -ConfigurationDirectory $PSScriptRoot\Output -FamilyName 'Calibri' -FontSize 16 -PositionX 100 -PositionY 100 -WallpaperFit Fit
