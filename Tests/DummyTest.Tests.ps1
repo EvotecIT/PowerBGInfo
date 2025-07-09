375f18a7fa6f533208c06a95854ff3f2b25023a4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-09 18:51:45 +0200

    chore(tests): 🧪 Add dummy test for validation
    * Introduced a new test file `DummyTest.Tests.ps1` to validate dummy functionality.
    * Updated `PowerBGInfo.csproj` to reflect changes in package versions and target frameworks.
    * Updated `PowerBGInfo.Tests.csproj` to use the latest versions of testing packages.

---
+++ Tests/DummyTest.Tests.ps1
@@ -0,0 +1,8 @@
+﻿Describe 'DummyTest' -Tag 'DummyTest' {
+    It 'Dummy Test' {
+        $dummy = 'This is a dummy test'
+        $dummy | Should -Not -BeNullOrEmpty
+        $dummy | Should -Be 'This is a dummy test'
+
+    }
+}
